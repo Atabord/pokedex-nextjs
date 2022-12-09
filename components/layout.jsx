b576a4f1e81ecabb7f748c7f8e4fdc115d105c18
Alejandro Taborda Diaz <alejandro.tabordad@perficient.com>
2022-12-09 14:57:22 -0500

    adding fallback

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import styles from '../styles/layout.module.css';
 
 export default function Layout({ children }) {
+    const router = useRouter();
+
+    if (router.isFallback) {
+      return <div>Loading...</div>
+    }
+
     return <div className={styles.container}>
         <header className={styles.header}>
             <Image
